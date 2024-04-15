5a4db090e7bfb5d8beba00faddf0e2481e9edb52
Sarah Cavanaugh <sarahcavanaugh20@gmail.com>
2024-04-15 11:04:37 -0500

    React fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import {
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
 import ViewAllJobs from './components/ViewAllJobs';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			index
+			elements={<h1>My</h1>}
+		/>
+	)
+);
 const App = () => {
 	return (
 		<div id='app-container'>
