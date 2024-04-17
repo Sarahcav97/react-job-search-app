a8d372232d295c90bed8c201a2d5e56203bce2fb
Sarah Cavanaugh <sarahcavanaugh20@gmail.com>
2024-04-17 16:01:16 -0500

    fixing hooks

--- client/src/pages/JobPage.jsx
+++ client/src/pages/JobPage.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const JobPage = () => {
-	return <div></div>;
+	const { id } = useParams();
+	const [job, setJob] = useState(null);
+	useEffect(() => {
+		const fetchJob = async () => {
+			try {
+				const res = await fetch(`/api/job/${id}`);
+				const data = await res.json();
+				setJob(data);
+			} catch (error) {
+				console.log('error fetching data', error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchJob();
+	}, []);
+	return <div>JobPage</div>;
 };
+
 export default JobPage;
